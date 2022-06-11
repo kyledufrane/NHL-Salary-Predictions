cd3d09342c89b9a769a97607ae0576e8b4eb9d1b
kyle <kyle.dufrane@aol.com>
2022-06-11 13:47:42 -0800

    updating dash

--- dash/app.py
+++ dash/app.py
@@ -1,7 +1,9 @@
 import dash
 import dash_bootstrap_components as dbc
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG],
+                          meta_tags=[{'name': 'viewport',
+                                      'content': 'width=device-width, initial-scale=1.0'}])
 
 server = app.server
 
