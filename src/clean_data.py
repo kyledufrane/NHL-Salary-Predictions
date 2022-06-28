6c1b317a8573bf072102b773f90875bb228b9390
kyledufrane <kyle.dufrane@gmail.com>
2022-06-28 09:50:06 -0800

    updating data

---
+++ src/clean_data.py
@@ -0,0 +1,146 @@
+
+# from matplotlib import pyplot as plt
+
+import pandas as pd
+
+import warnings
+warnings.filterwarnings('ignore')
+
+def clean_data():
+    df = pd.read_csv('https://raw.githubusercontent.com/kyledufrane/NHL-Salary-Predictions/main/data/ml_cleaned_forward_players_df.csv')
+
+    drop = [
+        'birthCity',
+        'birthDate',
+        'Unnamed: 0',
+        'index',
+        'birthStateProvince',
+        'birthCountry',
+        'height'
+    ]
+
+    df.drop(drop, axis=1, inplace=True)
+
+    years = ['17', '18', '19', '20', '21', '22']
+    columns_ = [col for col in df.select_dtypes(['int', 'float']).columns 
+                if 'Salary' not in col and 'Pct' not in col
+                if '22' in col
+                or '21' in col
+                or '20' in col
+                or '19' in col
+                or '18' in col
+                or '17' in col]
+
+    cleaned_names = [col_name[:-2] for col_name in columns_]
+    unique_names = list(set(cleaned_names))
+    shortHandedPoints = []
+    powerPlayGoals = []
+    powerPlayTimeOnIcePerGame = []
+    shortHandedTimeOnIce = []
+    shortHandedGoals = []
+    evenTimeOnIce = []
+    shortHandedTimeOnIcePerGame = []
+    penaltyMinutes = []
+    gameWinningGoals = []
+    timeOnIcePerGame = []
+    assists = []
+    blocked = []
+    points = []
+    hits = []
+    shifts = []
+    powerPlayTimeOnIce = []
+    plusMinus = []
+    powerPlayPoints = []
+    evenTimeOnIcePerGame = []
+    goals = []
+    overTimeGoals = []
+    pim = []
+    games = []
+    timeOnIce = []
+    shots = []
+
+    for year in years:
+        for col_name in unique_names:
+            if col_name == 'shortHandedPoints':
+                shortHandedPoints.append(f'{col_name}{year}')
+            elif col_name == 'powerPlayGoals':
+                powerPlayGoals.append(f'{col_name}{year}')
+            elif col_name == 'powerPlayTimeOnIcePerGame':
+                powerPlayTimeOnIcePerGame.append(f'{col_name}{year}')
+            elif col_name == 'shortHandedTimeOnIce':
+                shortHandedTimeOnIce.append(f'{col_name}{year}')
+            elif col_name == 'shortHandedGoals':
+                shortHandedGoals.append(f'{col_name}{year}')
+            elif col_name == 'evenTimeOnIce':
+                evenTimeOnIce.append(f'{col_name}{year}')
+            elif col_name == 'shortHandedTimeOnIcePerGame':
+                shortHandedTimeOnIcePerGame.append(f'{col_name}{year}')
+            elif col_name == 'penaltyMinutes':
+                penaltyMinutes.append(f'{col_name}{year}')
+            elif col_name == 'gameWinningGoals':
+                gameWinningGoals.append(f'{col_name}{year}')
+            elif col_name == 'timeOnIcePerGame':
+                timeOnIcePerGame.append(f'{col_name}{year}')
+            elif col_name == 'assists':
+                assists.append(f'{col_name}{year}')
+            elif col_name == 'blocked':
+                blocked.append(f'{col_name}{year}')
+            elif col_name == 'points':
+                points.append(f'{col_name}{year}')
+            elif col_name == 'hits':
+                hits.append(f'{col_name}{year}')
+            elif col_name == 'shifts':
+                shifts.append(f'{col_name}{year}')
+            elif col_name == 'powerPlayTimeOnIce':
+                powerPlayTimeOnIce.append(f'{col_name}{year}')
+            elif col_name == 'plusMinus':
+                plusMinus.append(f'{col_name}{year}')
+            elif col_name == 'powerPlayPoints':
+                powerPlayPoints.append(f'{col_name}{year}')
+            elif col_name == 'evenTimeOnIcePerGame':
+                evenTimeOnIcePerGame.append(f'{col_name}{year}')
+            elif col_name == 'goals':
+                goals.append(f'{col_name}{year}')
+            elif col_name == 'overTimeGoals':
+                overTimeGoals.append(f'{col_name}{year}')
+            elif col_name == 'pim':
+                pim.append(f'{col_name}{year}')
+            elif col_name == 'games':
+                games.append(f'{col_name}{year}')
+            elif col_name == 'timeOnIce':
+                timeOnIce.append(f'{col_name}{year}')
+            else:
+                shots.append(f'{col_name}{year}')
+
+    col_filters = [shortHandedPoints,
+                    powerPlayGoals,
+                    powerPlayTimeOnIcePerGame,
+                    shortHandedTimeOnIce,
+                    shortHandedGoals,
+                    evenTimeOnIce,
+                    shortHandedTimeOnIcePerGame,
+                    penaltyMinutes,
+                    gameWinningGoals,
+                    timeOnIcePerGame,
+                    assists,
+                    blocked,
+                    points,
+                    hits,
+                    shifts,
+                    powerPlayTimeOnIce,
+                    plusMinus,
+                    powerPlayPoints,
+                    evenTimeOnIcePerGame,
+                    goals,
+                    overTimeGoals,
+                    pim,
+                    games,
+                    timeOnIce,
+                    shots,]
+
+    for filter in col_filters:
+        temp_df = df[filter]
+        df[f'career_{filter[0]}'] = temp_df.sum(axis=1)
+    
+    return df
+
--- src/modeling.py
+++ src/modeling.py
@@ -1,11 +1,11 @@
-from sklearn.preprocessing import StandardScaler, OneHotEncoder, RobustScaler
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.impute import SimpleImputer
-from sklearn.metrics import roc_auc_score, f1_score, accuracy_score, classification_report
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier
+from sklearn.metrics import mean_squared_error
+# from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.compose import ColumnTransformer 
 from sklearn.pipeline import Pipeline
-from xgboost import XGBClassifier
+from xgboost import XGBRegressor
 
 import numpy as np
 import pandas as pd
@@ -15,24 +15,22 @@ import pandas as pd
 def baseline_modeling_pipeline(X_train, X_test, y_train, y_test, model):
     
     if model == 'randomforest':
-        model = RandomForestClassifier()
-    elif model == 'extratrees':
-        model = ExtraTreesClassifier()
+        model = RandomForestRegressor()
     else:
-        model = XGBClassifier()
+        model = XGBRegressor()
 
-    # numeric_features = X_train.select_dtypes(
-    #     ['int64', 'float64']).columns.tolist()
-    # categorical_features = X_train.select_dtypes(
-    #     ['object', 'bool']).columns.tolist()
+    numeric_features = X_train.select_dtypes(
+        ['int64', 'float64']).columns.tolist()
+    categorical_features = X_train.select_dtypes(
+        ['object', 'bool']).columns.tolist()
 
     numeric_transformer = Pipeline(steps=[('scaler', StandardScaler())])
 
     categorical_transformer = OneHotEncoder(handle_unknown='ignore')
 
     preprocessor = ColumnTransformer(
-        transformers=[('num', numeric_transformer, X_train),])
-                     # ('cat', categorical_transformer, categorical_features)])
+        transformers=[('num', numeric_transformer, X_train),
+                     ('cat', categorical_transformer, categorical_features)])
 
     clf = Pipeline(steps=[('preprocessor', preprocessor),
                           ('classifier', model)])
@@ -41,18 +39,17 @@ def baseline_modeling_pipeline(X_train, X_test, y_train, y_test, model):
 
     y_hat = clf.predict(X_test)
 
-    print(classification_report(y_test, y_hat))
 
-    # try:
+    try:
 
-    #     onehot_columns = clf.named_steps['preprocessor'] \
-    #                             .named_transformers_['cat'] \
-    #                             .get_feature_names(input_features=categorical_features)
+        onehot_columns = clf.named_steps['preprocessor'] \
+                                .named_transformers_['cat'] \
+                                .get_feature_names(input_features=categorical_features)
 
-    #     feature_importance = pd.DataFrame(data=clf.named_steps['classifier'].feature_importances_,
-    #                             index=np.array(numeric_features + list(onehot_columns))) \
-    #                             .sort_values(0, ascending=False)
-    # except:
-    #     feature_importance = []
+        feature_importance = pd.DataFrame(data=clf.named_steps['classifier'].feature_importances_,
+                                index=np.array(numeric_features + list(onehot_columns))) \
+                                .sort_values(0, ascending=False)
+    except:
+        feature_importance = []
 
-    return clf # , feature_importance
+    return clf, feature_importance
