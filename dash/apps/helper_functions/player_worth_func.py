a33ab4a8af0f6e396efb40c31789b298e2740c99
kyledufrane <kyle.dufrane@gmail.com>
2022-06-30 12:30:48 -0800

    testing

---
+++ dash/apps/helper_functions/player_worth_func.py
@@ -0,0 +1,69 @@
+from dash import html, Input, Output, dcc
+import dash_bootstrap_components as dbc
+
+def add_header(header):
+    return html.H4(
+        header,
+        style={
+            'textAlign': 'center',
+            'text-decoration': 'underline'
+        },
+        className='py-3'
+    )
+
+
+def add_slider_input(data):
+    return dbc.Row([
+        dbc.Col(
+            dcc.Slider(
+                id=f'{data}_slider',
+                min=0,
+                max=df[data].max() * 1.25,
+                value=round(df[data].mean()),
+                step=1,
+                marks=None,
+                className='my-2'
+            ),
+            style={
+                'textAlign': 'left'
+            }
+        ),
+        dbc.Col(
+            dcc.Input(
+                id=f"{data}_input",
+                type='number',
+                value=round(df[data].mean()),
+                style={
+                    'width': '50%'
+                }
+            ),
+        )
+    ])
+
+
+def convert_dash_format(layout):
+    convert = ()
+    for i in range(len(layout)):
+        convert += layout[i]
+    return convert
+
+
+def slider_input_update(slider_val, input_val, old_slider_val, old_input_val):
+    if slider_val != input_val:
+        if slider_val == old_slider_val:
+            updated_slider_val = input_val
+            updated_input_val = input_val
+        else:
+            updated_input_val = slider_val
+            updated_slider_val = slider_val
+        return updated_slider_val, updated_input_val
+
+
+def check_for_update(slider, input, slider_output, input_output):
+    if slider != input:
+        slider_updated, input_updated = slider_input_update(
+            slider, input, slider_output, input_output)
+    else:
+        slider_updated = slider
+        input_updated = input
+    return slider_updated, input_updated
--- dash/apps/player_worth.py
+++ dash/apps/player_worth.py
@@ -4,6 +4,7 @@ import pandas as pd
 import pickle
 
 from app import app
+from helper_functions import player_worth_func
 
 import sys
 sys.path.append('/home/kyle/Desktop/NHL-Salary-Predictions')
@@ -35,75 +36,6 @@ powerPlayTimeOnIcePerGame22_slider_output = int()
 powerPlayTimeOnIcePerGame22_input_output = int()
 assists22_slider_output = int()
 assists22_input_output = int()
-
-
-def add_header(header):
-    return html.H4(
-        header,
-        style={
-            'textAlign': 'center',
-            'text-decoration': 'underline'
-        },
-        className='py-3'
-    )
-
-
-def add_slider_input(data):
-    return dbc.Row([
-        dbc.Col(
-            dcc.Slider(
-                id=f'{data}_slider',
-                min=0,
-                max=df[data].max() * 1.25,
-                value=round(df[data].mean()),
-                step=1,
-                marks=None,
-                className='my-2'
-            ),
-            style={
-                'textAlign': 'left'
-            }
-        ),
-        dbc.Col(
-            dcc.Input(
-                id=f"{data}_input",
-                type='number',
-                value=round(df[data].mean()),
-                style={
-                    'width': '50%'
-                }
-            ),
-        )
-    ])
-
-
-def convert_dash_format(layout):
-    convert = ()
-    for i in range(len(layout)):
-        convert += layout[i]
-    return convert
-
-
-def slider_input_update(slider_val, input_val, old_slider_val, old_input_val):
-    if slider_val != input_val:
-        if slider_val == old_slider_val:
-            updated_slider_val = input_val
-            updated_input_val = input_val
-        else:
-            updated_input_val = slider_val
-            updated_slider_val = slider_val
-        return updated_slider_val, updated_input_val
-
-
-def check_for_update(slider, input, slider_output, input_output):
-    if slider != input:
-        slider_updated, input_updated = slider_input_update(
-            slider, input, slider_output, input_output)
-    else:
-        slider_updated = slider
-        input_updated = input
-    return slider_updated, input_updated
-
 
 column_one_map = {
     'Career Assists': 'career_assists',
